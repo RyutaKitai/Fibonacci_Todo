d7b2b78f9674efb4fc4e25957708ef7a3f5613dd
RyutaKitai <ryuuta.k@icloud.com>
2021-03-01 00:53:25 +1000

    add Adverstisement

--- src/screens/MemoToppage.js
+++ src/screens/MemoToppage.js
@@ -10,23 +10,34 @@ class MemoToppage extends React.Component {
     // const db = SQLiteData();
     // const data = SQLiteShow();
     return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Top画面</Text>
-        <Image
-          style={{ width: 200, height: 200, marginVertical: 30 }}
-          resizeMode="contain"
-          // eslint-disable-next-line global-require
-          source={require('../../assets/fibo-todo2.png')}
-        />
-        <Button onPress={() => { this.props.navigation.navigate('Memolist'); }}>
-          メモする
-        </Button>
+      <View style={styles.whole}>
+        <View style={styles.container}>
+          <Text style={styles.title}>Top画面</Text>
+          <Image
+            style={{ width: 200, height: 200, marginVertical: 30 }}
+            resizeMode="contain"
+            // eslint-disable-next-line global-require
+            source={require('../../assets/fibo-todo2.png')}
+          />
+          <Button onPress={() => { this.props.navigation.navigate('Memolist'); }}>
+            メモする
+          </Button>
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>
+            広告
+          </Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  whole: {
+    height: '100%',
+    width: '100%',
+  },
   container: {
     margin: 30,
     marginTop: 10,
@@ -39,6 +50,19 @@ const styles = StyleSheet.create({
     paddingHorizontal: 120,
     fontSize: 20,
     marginVertical: 40,
+  },
+  footer: {
+    position: 'absolute',
+    alignItems: 'center',
+    bottom: 30,
+    borderWidth: 1,
+    // paddingHorizontal: 210,
+  },
+  footerText: {
+    padding: 10,
+    paddingHorizontal: 210,
+    paddingVertical: 20,
+    fontSize: 20,
   },
 });
 
